ec5e60f417422f1d592c5e116d0739d6859be1b3
riyagarasangi <157198559+riyagarasangi@users.noreply.github.com>
2025-08-07 16:32:08 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-cd /home/ec2-user/webapp
-nohup python3 app.py > app.log 2>&1 &
+echo "Starting server..."
+cd /home/ec2-user/my_webserver
+# Replace with your actual app entry point
+nohup gunicorn --bind 0.0.0.0:5000 app:app > /dev/null 2>&1 &
